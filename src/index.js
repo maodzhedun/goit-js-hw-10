cf923a9939cc7ccce7d5726212bfe71605ec6b23
maodzhedun <maodzhedun@gmail.com>
2023-08-11 07:43:36 +0300

    selest

--- src/index.js
+++ src/index.js
@@ -4,6 +4,36 @@ import { Report } from 'notiflix/build/notiflix-report-aio';
 
 import { fetchBreeds } from './js/cat-api';
 
+const refs = {
+    selectForm: document.querySelector(".breed-select"),
+    loaderEl: document.querySelector(".loader"),
+    errorEl: document.querySelector(".error"),
+    catInfoBlock: document.querySelector(".cat-info"),
+};
+const { selectForm, loaderEl, errorEl, catInfoBlock } = refs;
 
 
-fetchBreeds().then(resp => console.log(resp.data))
+
+refs.selectForm.addEventListener("change", createCardCat);
+
+
+fetchBreeds()
+    .then(array => { return selectForm.innerHTML = createMarkup(array.data)})
+    .catch((err) => console.log(err));
+
+
+fetchCatByBreed(breedId)
+    .then(resp => console.log(resp))
+    .catch((err) => console.log(err));
+
+function createCardCat(event) {
+    
+}
+
+function createMarkup(array) {
+    return array.map(({id, name}) => `<option value="${id}">${name}</option>`).join("");
+}
+
+function createMarkupCat() {
+    
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const BASE_URL = "https://api.thecatapi.com/v1/breeds";
+const SEARCH_URL = "https://api.thecatapi.com/v1/images/search";
 const KEY = "live_plbpvaVGQwBu0yyFGTPnCrW5wc66OmE2nGTiZMgzZpirFCJyo2XSa6k0DY9qG42m";
 
 axios.defaults.headers.common["x-api-key"] = KEY;
@@ -9,4 +10,8 @@ function fetchBreeds(){
 return axios.get(BASE_URL)
 }
 
-export{ fetchBreeds }
+function fetchCatByBreed(breedId){ 
+    return axios.get(`${SEARCH_URL}?breed_ids=${breed_ids}`)
+}
+
+export{ fetchBreeds, fetchCatByBreed }
